6c3abb4b1bbd91fa6782719089ae9263d226536e
Darley36 <darleybest@gmail.com>
2021-10-13 09:38:55 -0500

    Service and test terminated

--- src/test/java/com/example/TestStepVeri/services/ServicioTest.java
+++ src/test/java/com/example/TestStepVeri/services/ServicioTest.java
@@ -7,6 +7,8 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -22,5 +24,55 @@ class ServicioTest {
     void testVarios() {
         Flux<String> uno = servicio.buscarTodos();
         StepVerifier.create(uno).expectNext("Pedro").expectNext("Maria").expectNext("Jesus").expectNext("Carmen").verifyComplete();
+    }
+    @Test
+    void testVariosLento() {
+        Flux<String> uno = servicio.buscarTodosLento();
+        StepVerifier.create(uno)
+                .expectNext("Pedro")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Maria")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Jesus")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Carmen")
+                .thenAwait(Duration.ofSeconds(1)).verifyComplete();
+    }
+
+    @Test
+    void testTodosFiltro() {
+        Flux<String> source = servicio.buscarTodosFiltro();
+        Flux<String> error = source.concatWith(
+                Mono.error(new IllegalArgumentException("Mensaje de Error"))
+        );
+        StepVerifier
+                .create(source)
+                .expectNextCount(4)
+                .expectErrorMatches(throwable -> throwable instanceof IllegalArgumentException &&
+                        throwable.getMessage().equals("Mensaje de Error")
+                ).verify();
+    }
+
+    @Test
+    void testTodo_other() {
+        Flux<Integer> source = Flux.<Integer>create(emitter -> {
+            emitter.next(1);
+            emitter.next(2);
+            emitter.next(3);
+            emitter.complete();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            emitter.next(4);
+        }).filter(number -> number % 2 == 0);
+        StepVerifier.create(source)
+                .expectNext(2)
+                .expectComplete()
+                .verifyThenAssertThat()
+                .hasDropped(4)
+                .tookLessThan(Duration.ofMillis(1050));
+
     }
 }
