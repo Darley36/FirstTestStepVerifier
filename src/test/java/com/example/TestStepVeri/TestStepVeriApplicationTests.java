e71ad2a0c9f9bd2c541f182908ccadc43c711b30
Darley36 <darleybest@gmail.com>
2021-10-13 08:47:48 -0500

    First commit

---
+++ src/test/java/com/example/TestStepVeri/TestStepVeriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestStepVeri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestStepVeriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
